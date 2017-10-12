82d7021d28e3dbf1eca123c2566ba29a7d1876df
Andrew Kramer <1988kramer@gmail.com>
2017-10-12 13:51:30 -0600

    finished with hw3

--- hw3/LSTM.py
+++ hw3/LSTM.py
@@ -1,8 +1,8 @@
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM, GRU
+from keras.layers import Dense, Conv1D, MaxPooling1D
+from keras.layers import LSTM, GRU, Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
@@ -10,7 +10,7 @@ class RNN:
     '''
     RNN classifier
     '''
-    def __init__(self, train_x, train_y, test_x, test_y, dict_size=20000, example_length=80, embedding_length=32, epochs=15, batch_size=32):
+    def __init__(self, train_x, train_y, test_x, test_y, dict_size=5000, example_length=500, embedding_length=128, epochs=2, batch_size=64):
         '''
         initialize RNN model
         :param train_x: training data
@@ -34,8 +34,11 @@ class RNN:
 
         # TODO:build model
         self.model = Sequential()
-        self.model.add(Embedding(dict_size, 128))
-        self.model.add(GRU(128, dropout=0.2, recurrent_dropout=0.2))
+        self.model.add(Embedding(self.dict_size, self.embedding_len, input_length=self.example_len))
+        self.model.add(Dropout(0.25))
+        self.model.add(Conv1D(64, 5, padding='valid', activation='relu', strides=1)) 
+        self.model.add(MaxPooling1D(pool_size=4)) 
+        self.model.add(LSTM(128, kernel_initializer='random_uniform', bias_initializer='zeros', dropout=0.2, recurrent_dropout=0.2))
         self.model.add(Dense(1, activation='sigmoid'))
 
         self.model.compile(loss='binary_crossentropy',
@@ -63,8 +66,11 @@ class RNN:
 
 
 if __name__ == '__main__':
-    (train_x, train_y), (test_x, test_y) = imdb.load_data(num_words=5000)
-    rnn = RNN(train_x, train_y, test_x, test_y)
+    max_words = 5000
+    example_len = 500
+    embedding_len = 128
+    (train_x, train_y), (test_x, test_y) = imdb.load_data(num_words=max_words)
+    rnn = RNN(train_x, train_y, test_x, test_y, max_words, example_len, embedding_len)
     rnn.train()
     score, acc = rnn.evaluate()
     print("score:", score, "accuracy:", acc)
